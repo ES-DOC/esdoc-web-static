90dfec412f89099ad5ef640b635e2e20296d5b92
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-02-20 20:13:09 +0100

    Latest plugin build

--- esdoc.js
+++ esdoc.js
@@ -1,14 +1,14 @@
 /*!
- * esdoc-js-client - javascript Library v0.9.5.3
+ * esdoc-js-client - javascript Library v0.9.7.0
  * https://github.com/ES-DOC/esdoc-js-client
  *
- * Copyright 2016, ES-DOC (http://es-doc.org)
+ * Copyright 2017, ES-DOC (http://es-doc.org)
  *
  * Licensed under the following licenses:.
  *     CeCILL       http://www.cecill.info/licences/Licence_CeCILL_V2-en.html
  *     GPL v3       http://www.gnu.org/licenses/gpl.html
  *
- * Date: Tue Nov 22 20:40:43 UTC 2016
+ * Date: Mon Feb 20 19:07:03 UTC 2017
  */
 
 /*! jQuery v1.9.1 | (c) 2005, 2012 jQuery Foundation, Inc. | jquery.org/license
@@ -407,7 +407,7 @@ return(!i||i!==r&&!b.contains(r,i))&&(e.type=o.origType,n=o.handler.apply(this,a
     // Initialise root object.
     root.CIM = root.cim = root.ESDOC = root.esdoc = {
         // App version.
-        VERSION: '0.9.5.3',
+        VERSION: '0.9.7.0',
 
         // App name.
         NAME: "Earth System Documentation",
@@ -531,7 +531,7 @@ return(!i||i!==r&&!b.contains(r,i))&&(e.type=o.origType,n=o.handler.apply(this,a
             ESDOC.options.staticBaseURL = 'http://static.es-doc.org';
         }
     } else {
-        ESDOC.options.staticBaseURL = '../../esdoc-static';
+        ESDOC.options.staticBaseURL = '../esdoc-web-static';
     }
 
     // Set custom options.
@@ -3374,7 +3374,7 @@ return(!i||i!==r&&!b.contains(r,i))&&(e.type=o.origType,n=o.handler.apply(this,a
             viewerBaseURL = 'http://view.es-doc.org';
         }
     } else {
-        viewerBaseURL = '../../esdoc-web/viewer/index.html';
+        viewerBaseURL = '../esdoc-web-view/index.html';
     }
 
     // Performs standard parameter validation.
@@ -4492,8 +4492,8 @@ return(!i||i!==r&&!b.contains(r,i))&&(e.type=o.origType,n=o.handler.apply(this,a
     ESDOC.comparator.constants = {
         // C1 comparator constants.
         c1 :{
-            TAB1_HELP_URL : "https://github.com/ES-DOC/esdoc-web/wiki/Comparator-Help",
-            TAB2_HELP_URL : "https://github.com/ES-DOC/esdoc-web/wiki/Comparator-Help"
+            TAB1_HELP_URL : "https://github.com/ES-DOC/esdoc-web-compare/wiki/CMIP5-Comparator-Help",
+            TAB2_HELP_URL : "https://github.com/ES-DOC/esdoc-web-compare/wiki/CMIP5-Comparator-Help"
         }
     };
 
